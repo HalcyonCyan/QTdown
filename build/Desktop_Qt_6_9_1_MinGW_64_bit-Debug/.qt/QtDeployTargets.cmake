ed78dcc9cfb227468a3e0cfe980e079d0993cabd
HalcyonCyan <1016969633@qq.com>
2025-09-07 19:04:40 +0800

    删除build

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DeepRock_FILE C:/Users/10169/Desktop/man100/1.2/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/DeepRock.exe)
-set(__QT_DEPLOY_TARGET_DeepRock_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DeepRock_RUNTIME_DLLS D:/qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;D:/qt/6.9.1/mingw_64/bin/Qt6Gui.dll;D:/qt/6.9.1/mingw_64/bin/Qt6Core.dll)
